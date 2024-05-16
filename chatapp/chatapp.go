0287fc9608b72d5fb695444c2c509b073247dd77
krishnaindani <indanikrishna@gmail.com>
2024-05-15 22:11:06 -0700

    cretauser and login

--- chatapp/chatapp.go
+++ chatapp/chatapp.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/glebarez/go-sqlite"
 	"github.com/rs/zerolog/log"
+	"net/http"
 	"os"
 )
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Channel struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Message struct {
+	ID        int    `json:"id"`
+	ChannelID int    `json:"channel_id"`
+	UserID    int    `json:"user_id"`
+	UserName  string `json:"user_name"`
+	Text      string `json:"text"`
+}
 
 func main() {
 
@@ -65,4 +86,53 @@ func main() {
 			Err(err).
 			Msg("error starting server")
 	}
+}
+
+func createUser(c *gin.Context, db *sql.DB) {
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	result, err := db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", user.Username, user.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id": id,
+	})
+}
+
+func login(c *gin.Context, db *sql.DB) {
+
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	row := db.QueryRow("SELECT * FROM users WHERE username = ?, user.Username AND password = ?", user.Username, user.Password)
+
+	var id int
+
+	err := row.Scan(&id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid username or password"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"id": id})
 }
